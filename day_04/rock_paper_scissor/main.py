9e2afe9055aeb69fa0d8516e47b82933a7fc75fc
Guilherme Oliveira <silva.guilhermeoliveira.com>
2023-05-03 17:26:48 -0300

    Game

---
+++ day_04/rock_paper_scissor/main.py
@@ -0,0 +1,42 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+sicssor = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+computer_choice = random.randint(0, 2)
+user_choice = int(
+    input("What do you choose:\n 0 - Rock\n 1 - Paper\n 2 - Scissor\n\n"))
+
+if user_choice not in (0, 1, 2):
+    print("Incorrect input")
+
+option = [rock, paper, sicssor]
+result = ['Drawn', "You Won", "You Lose"]
+
+x = user_choice - computer_choice
+
+print(f"\nYou choose {option[user_choice]}\n\n Computer Choose {option[computer_choice]}\n\n {result[x]}")
